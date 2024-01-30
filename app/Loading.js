5d4ed38a098dea734b1b477e44ec526d501cce0c
Mohamad Imani <imani.mohamad98@gmail.com>
2024-01-30 14:30:09 +0330

    Change style

--- app/Loading.js
+++ app/Loading.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div className='font-semibold text-2xl text-purple-600'>...Loading</div>
+    <div className='font-semibold text-2xl text-[#035d9a]'>...Loading</div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -2,8 +2,12 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
+import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+const myFont = localFont({ src: "./Nazanin.ttf" });
 
-export default function Home() {
+export default function Home({ pageProps }) {
   const [allPosts, setAllPosts] = useState([]);
   const [posts, setPosts] = useState([]);
   const [id, setId] = useState("");
@@ -58,11 +62,16 @@ export default function Home() {
     );
   }
   return (
-    <main className=" h-screen bg-slate-800">
+    <main className={`h-screen  bg-gradient-to-t from-slate-300 to-slate-500 ${myFont.className}`}>
       <div className="flex ">
-        <div className="flex justify-between w-full border-black rounded-md shadow-md shadow-purple-300 p-5 m-5 bg-gradient-to-r from-violet-200 to-pink-200">
-          <div className="space-y-5">
-            <h1 className="font-bold text-2xl text-slate-700">Blogs</h1>
+      
+        <div className="flex justify-between w-full border-black rounded-md shadow-sm shadow-black/50 p-5 m-5 bg-white/65">
+          <div className="space-y-5 flex flex-col items-center">
+            <h1
+              className={`font-bold text-3xl text-[#035d9a] `}
+            >
+              بلاگ ها
+            </h1>
 
             <ol>
               {isLoading ? (
@@ -71,7 +80,7 @@ export default function Home() {
                 allPosts.map((post) => (
                   <li key={post.id}>
                     <Link
-                      className="font-medium hover:text-purple-500"
+                      className="font-semibold transition-all duration-300 hover:text-[#035d9a] text-lg text-black text-opacity-80 "
                       href={post.link}
                     >
                       {post.title.rendered}
@@ -81,15 +90,27 @@ export default function Home() {
               )}
             </ol>
           </div>
-          <div className="space-y-5">
-            <h1 className="font-bold text-2xl text-slate-700">Search blog by ID</h1>
-            <form action="" onSubmit={handleSubmit}>
-              <input type="number" />
+          <div className="w-[1px] h-[90%] my-auto bg-black/50"></div>
+          <div className="space-y-5 flex flex-col items-center">
+            <h1
+              className={`font-bold text-3xl text-[#035d9a]`}
+            >
+              جستجوی بلاگ بر اساس شناسه
+            </h1>
+            <form
+              action=""
+              onSubmit={handleSubmit}
+              className="flex justify-center items-center"
+            >
+              <input
+                type="number"
+                className="bg-slate-200 rounded-md px-2 py-1 order-1 focus:outline-0"
+              />
               <button
                 type="submit"
-                className="px-2 border font-medium mx-1 rounded-md hover:text-purple-500"
+                className=" px-2 py-1  font-light mx-1 rounded-md hover:bg-opacity-40 bg-[#01b47e] transition-all duration-300"
               >
-                Search
+                🔎
               </button>
             </form>
             {error ? (
@@ -97,7 +118,7 @@ export default function Home() {
             ) : (
               posts.map((postById) => (
                 <Link
-                  className="font-medium hover:text-purple-500"
+                  className="font-semibold transition-all duration-300 hover:text-[#035d9a] text-lg text-black text-opacity-80 "
                   key={postById.id}
                   href={postById.link}
                 >
