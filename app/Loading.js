6865b6fb3c0ce4387d26a0ab66ea5ae62ae7c5fe
Mohamad Imani <imani.mohamad98@gmail.com>
2024-01-30 13:09:18 +0330

    initial commit

---
+++ app/Loading.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className='font-semibold text-2xl text-purple-600'>...Loading</div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,112 +1,112 @@
-import Image from "next/image";
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 export default function Home() {
+  const [allPosts, setAllPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+  const [id, setId] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getPosts() {
+      setIsLoading(true);
+      const res = await fetch("https://vahidalvandi.ir/wp-json/wp/v2/posts");
+      const data = await res.json();
+      setAllPosts(data);
+      setIsLoading(false);
+    }
+    getPosts();
+  }, []);
+
+  useEffect(() => {
+    async function getPostById() {
+      try {
+        if (id) {
+          const res = await fetch(
+            `https://vahidalvandi.ir/wp-json/wp/v2/posts/${id}`
+          );
+          if (!res.ok) throw new Error("Wrong ID");
+          const data = await res.json();
+          setPosts([data]);
+        } else {
+          setPosts([]);
+        }
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+      }
+    }
+    getPostById();
+    return () => {
+      setError("");
+    };
+  }, [id]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setId(e.target.querySelector("input").value);
+  }
+  function ErrorMessage({ message }) {
+    return (
+      <p>
+        <span>⛔ </span>
+
+        {message}
+      </p>
+    );
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=" h-screen bg-slate-800">
+      <div className="flex ">
+        <div className="flex justify-between w-full border-black rounded-md shadow-md shadow-purple-300 p-5 m-5 bg-gradient-to-r from-violet-200 to-pink-200">
+          <div className="space-y-5">
+            <h1 className="font-bold text-2xl text-slate-700">Blogs</h1>
+
+            <ol>
+              {isLoading ? (
+                <Loading />
+              ) : (
+                allPosts.map((post) => (
+                  <li key={post.id}>
+                    <Link
+                      className="font-medium hover:text-purple-500"
+                      href={post.link}
+                    >
+                      {post.title.rendered}
+                    </Link>
+                  </li>
+                ))
+              )}
+            </ol>
+          </div>
+          <div className="space-y-5">
+            <h1 className="font-bold text-2xl text-slate-700">Search blog by ID</h1>
+            <form action="" onSubmit={handleSubmit}>
+              <input type="number" />
+              <button
+                type="submit"
+                className="px-2 border font-medium mx-1 rounded-md hover:text-purple-500"
+              >
+                Search
+              </button>
+            </form>
+            {error ? (
+              <ErrorMessage message={error} />
+            ) : (
+              posts.map((postById) => (
+                <Link
+                  className="font-medium hover:text-purple-500"
+                  key={postById.id}
+                  href={postById.link}
+                >
+                  {postById.title.rendered}
+                </Link>
+              ))
+            )}
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
