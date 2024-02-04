c8a948b2e4f62a478d1eedcfe48e8bf8b621bd7a
Mohamad Imani <imani.mohamad98@gmail.com>
2024-02-04 12:49:50 +0330

    Change next.config domains to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["vahidalvandi.ir"],
+    remotePatterns: [
+      {
+        hostname: "vahidalvandi.ir",
+      },
+    ],
   },
 };
 
